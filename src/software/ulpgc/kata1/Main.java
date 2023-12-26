9e7694c8f50924ce9575ff589512b94efd517dee
Mauro Gómez <m.gomeznitoni@gmail.com>
2023-12-26 01:17:59 +0000

    Final Commit (Class Main finished)

--- src/software/ulpgc/kata1/Main.java
+++ src/software/ulpgc/kata1/Main.java
@@ -1,4 +1,10 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Mauro", LocalDate.of(2003, 4, 1));
+        System.out.println(person);
+    }
 }
